39d99cc3aa63f0ec49074254a16b760ce1029a7b
Osaretin Frank <osaretin.frank10@gmail.com>
2024-02-04 16:21:58 +0100

    add landing bag

---
+++ components/Logo.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+import Image from "next/image"
+import Link from "next/link"
+
+export const Logo = () => {
+  return (
+    <Link href="/">
+      <Image src={"/ied.png"} alt="IED Hackathon" width={200} height={200} />
+    </Link>
+  )
+}
