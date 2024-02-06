6f1a216108ae6c406e13d9c8843b2ba5e746d197
Osaretin Frank <osaretin.frank10@gmail.com>
2024-02-06 09:16:53 +0100

    add beams-bg

--- app/school/[id]/page.tsx
+++ app/school/[id]/page.tsx
@@ -1,9 +1,13 @@
 import { Header } from "@/components/Header"
+import Image from "next/image"
 import React from "react"
 
 const SchoolPage = () => {
   return (
-    <div className="bg-grey-50 m-2 p-[2rem] rounded-md urbanist min-h-[98vh]">
+    <div className="bg-[#cfcfcf67] m-2 p-[2rem] rounded-md urbanist min-h-[98vh] relative overflow-hidden">
+      <div className="absolute left-1/2 top-0 -ml-[39rem] w-[113.125rem] max-w-none h-[40rem] z-[-1]">
+        <Image fill alt="beams" src={"/beams-basic.png"} />
+      </div>
       <Header />
     </div>
   )
