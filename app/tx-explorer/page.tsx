df8a2719562c45a3986cb99014a892109854e6d5
Osaretin Frank <osaretin.frank10@gmail.com>
2024-02-07 03:53:56 +0100

    add transaction page

---
+++ app/tx-explorer/page.tsx
@@ -0,0 +1,42 @@
+"use client"
+import { Footer } from "@/components/Footer"
+import { Header } from "@/components/Header"
+import { Breadcrumb } from "antd"
+import Image from "next/image"
+import React from "react"
+import { BiHomeAlt } from "react-icons/bi"
+import { FaWpexplorer } from "react-icons/fa"
+
+const items = [
+  {
+    href: "/",
+    title: (
+      <div className="inline-flex items-center gap-2">
+        <BiHomeAlt className="inline" />
+      </div>
+    ),
+  },
+  {
+    title: (
+      <div className="flex items-center gap-2">
+        <FaWpexplorer />
+        <span>Tx Explorer</span>
+      </div>
+    ),
+  },
+]
+
+export default function TxExplorerPage() {
+  return (
+    <div>
+      <div className="bg-[#cfcfcf67] md:m-2 md:p-[2rem] p-[1rem] md:rounded-md urbanist min-h-[98vh] relative overflow-hidden">
+        <div className="absolute left-1/2 top-0 -ml-[39rem] w-[113.125rem] max-w-none h-[40rem] z-[-1]">
+          <Image fill alt="beams" src={"/beams-basic.png"} />
+        </div>
+        <Header />
+        <Breadcrumb items={items} className="my-[2rem]" />
+      </div>
+      <Footer />
+    </div>
+  )
+}
