a29fdc4d863795ceb6b72a6cce22981179f71100
Osaretin frank <Osaretin.frank10@gmail.com>
2024-02-20 10:18:33 +0100

    add nav bar animation

--- components/new_landingpage/Header.tsx
+++ components/new_landingpage/Header.tsx
@@ -1,20 +1,27 @@
-import Image from "next/image"
+"use client"
 import Link from "next/link"
-import React from "react"
+import React, { useEffect, useRef } from "react"
+import { NavMenu } from "./NavMenu"
+import { useGSAP } from "@gsap/react"
+import { gsap } from "gsap"
+import CSSRulePlugin from "gsap/CSSRulePlugin"
 
 export const Header = () => {
+  const navRef = React.useRef(null)
+  const [activeHam, setActiveHam] = React.useState(false)
+
   return (
     <div>
-      <div className="main-header text-white border-b-grey-700 border-b-[1px] px-[6rem] py-6">
-        <div className="flex justify-between items-center w-full">
+      <div className="main-header text-white border-b-grey-700 border-b-[1px] md:px-[6rem] px-[1rem] py-6">
+        <div className="flex justify-between items-center w-full ">
           <div className="flex gap-7 items-center">
-            <span className="text-xl">
+            <span className="text-xl relative z-[110]">
               <span>
                 i<span className="text-red-500">ED </span>
               </span>
               Hackathon
             </span>
-            <nav>
+            <nav className="md:block hidden">
               <ul className="flex gap-4 text-grey-200">
                 <li>
                   <Link href={""}>Docs</Link>
@@ -28,15 +35,32 @@ export const Header = () => {
               </ul>
             </nav>
           </div>
-          <div className="inline-flex flex-col gap-3 cursor-pointer">
-            <div className="w-10 h-[1px] bg-grey-300" />
-            <div className="w-10 h-[1px] bg-grey-300" />
+          <div
+            className="relative gap-3 cursor-pointer z-[110] p-5 -mb-10 hamburger"
+            onClick={() => setActiveHam(!activeHam)}
+          >
+            <div
+              className={`w-10 h-[1px] bg-grey-300 absolute top-0 left-0 z-[110] transition-all duration-300 ${
+                activeHam
+                  ? "translate-y-[0] rotate-[45deg]"
+                  : "translate-y-[-5px]"
+              }`}
+            />
+            <div
+              className={`w-10 h-[1px] bg-grey-300 absolute top-0 left-0 z-[110] transition-all duration-300 ${
+                activeHam
+                  ? "translate-y-[0] rotate-[-45deg]"
+                  : "translate-y-[5px]"
+              }`}
+            />
           </div>
         </div>
       </div>
       {/* GLOW */}
       <div className="glow w-[20rem] h-[25rem] bg-transperant  absolute top-0 left-[30%] translate-y-[-115%]"></div>
       {/* Tag Text */}
+      {/* {activeHam && <NavMenu active={activeHam} />} */}
+      <NavMenu active={activeHam} />
     </div>
   )
 }
---
+++ components/new_landingpage/NavMenu.tsx
@@ -0,0 +1,93 @@
+"use client"
+import Link from "next/link"
+import React, { useEffect, useRef } from "react"
+import { useGSAP } from "@gsap/react"
+import { gsap } from "gsap"
+import CSSRulePlugin from "gsap/CSSRulePlugin"
+import { BsGithub, BsInfoCircleFill } from "react-icons/bs"
+
+const activeItemIndicator = CSSRulePlugin.getRule(
+  ".menu-item .acive-route::after"
+)
+
+export const NavMenu = ({ active }: { active: boolean }) => {
+  const navRef = React.useRef(null)
+  const subNavRef = React.useRef(null)
+  const timeline = useRef<any>()
+
+  useGSAP(() => {
+    gsap.set(".menu-item p", { y: 225 })
+    timeline.current = gsap.timeline({ paused: true })
+    timeline.current.to(navRef.current, {
+      duration: 1.5,
+      clipPath: "polygon(0 0, 100% 0, 100% 100%, 0% 100%)",
+      ease: "power4.inOut",
+    })
+    timeline.current.to(
+      ".menu-item p",
+      { duration: 1.5, y: 0, stagger: 0.2, ease: "power4.out" },
+      "-=1"
+    )
+    timeline.current.to(
+      activeItemIndicator,
+      {
+        width: "100%",
+        duration: 1,
+        ease: "power4.out",
+        delay: 0.5,
+      },
+      "<"
+    )
+    timeline.current.to(
+      subNavRef.current,
+      { duration: 1, bottom: "10%", opacity: 1, delay: 0.5 },
+      "<"
+    )
+  })
+  useGSAP(() => {
+    if (active) {
+      timeline.current.play()
+    } else {
+      timeline.current.reverse()
+    }
+  }, [active])
+  return (
+    <div className="nav-overlay" ref={navRef}>
+      <div className="nav-overlay-menu">
+        <div className="menu-item">
+          <p className="active-route">
+            <Link href={""}>Home</Link>
+          </p>
+        </div>
+        <div className="menu-item">
+          <p className="">
+            <Link href={""}>Schools</Link>
+          </p>
+        </div>
+        <div className="menu-item">
+          <p className="">
+            <Link href={""}>TX Explorer</Link>
+          </p>
+        </div>
+      </div>
+      <div className="sub-nav grotesk" ref={subNavRef}>
+        <p>
+          <Link
+            href={""}
+            className="flex items-center gap-2 hover:underline transition-all duration-[0.3s]"
+          >
+            <BsGithub /> <span>Github Repository</span>
+          </Link>
+        </p>
+        <p>
+          <Link
+            href={""}
+            className="flex items-center gap-2 hover:underline transition-all duration-[0.3s]"
+          >
+            <BsInfoCircleFill /> <span>About the Hackathon</span>
+          </Link>
+        </p>
+      </div>
+    </div>
+  )
+}
