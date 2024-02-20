53c1ba1a7fdcf69c2280897406d226d8e5b3bd12
Osaretin frank <Osaretin.frank10@gmail.com>
2024-02-20 10:32:07 +0100

    updates

--- components/new_landingpage/NavMenu.tsx
+++ components/new_landingpage/NavMenu.tsx
@@ -6,11 +6,10 @@ import { gsap } from "gsap"
 import CSSRulePlugin from "gsap/CSSRulePlugin"
 import { BsGithub, BsInfoCircleFill } from "react-icons/bs"
 
-const activeItemIndicator = CSSRulePlugin.getRule(
-  ".menu-item .acive-route::after"
-)
-
 export const NavMenu = ({ active }: { active: boolean }) => {
+  // const activeItemIndicator = CSSRulePlugin.getRule(
+  //   ".menu-item .acive-route::after"
+  // )
   const navRef = React.useRef(null)
   const subNavRef = React.useRef(null)
   const timeline = useRef<any>()
@@ -19,7 +18,7 @@ export const NavMenu = ({ active }: { active: boolean }) => {
     gsap.set(".menu-item p", { y: 225 })
     timeline.current = gsap.timeline({ paused: true })
     timeline.current.to(navRef.current, {
-      duration: 1.5,
+      duration: 1.6,
       clipPath: "polygon(0 0, 100% 0, 100% 100%, 0% 100%)",
       ease: "power4.inOut",
     })
@@ -28,16 +27,16 @@ export const NavMenu = ({ active }: { active: boolean }) => {
       { duration: 1.5, y: 0, stagger: 0.2, ease: "power4.out" },
       "-=1"
     )
-    timeline.current.to(
-      activeItemIndicator,
-      {
-        width: "100%",
-        duration: 1,
-        ease: "power4.out",
-        delay: 0.5,
-      },
-      "<"
-    )
+    // timeline.current.to(
+    //   activeItemIndicator,
+    //   {
+    //     width: "100%",
+    //     duration: 1,
+    //     ease: "power4.out",
+    //     delay: 0.5,
+    //   },
+    //   "<"
+    // )
     timeline.current.to(
       subNavRef.current,
       { duration: 1, bottom: "10%", opacity: 1, delay: 0.5 },
