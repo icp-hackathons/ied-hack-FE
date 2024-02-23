fa224abfffcb67bd709f968128f187086890ce98
Osaretin frank <Osaretin.frank10@gmail.com>
2024-02-23 01:03:29 +0100

    new landing page

--- app/landing-2/page.tsx
+++ app/landing-2/page.tsx
@@ -1,15 +1,10 @@
 "use client"
 import { Counter } from "@/components/Landing/Counter"
 import { Announcement } from "@/components/new_landingpage/Announcement"
-import { Header } from "@/components/new_landingpage/Header"
 import { Hero } from "@/components/new_landingpage/Hero"
 import React from "react"
-import { gsap } from "gsap"
-import { useGSAP } from "@gsap/react"
 import { Showcase } from "./Showcase"
-import Gallery from "@/components/Gallary"
 import { Footer } from "@/components/Footer"
-// import { ImageRevealOnScroll } from "./Test"
 
 const LandingPage = () => {
   const [counter, setCounter] = React.useState(0)
@@ -23,10 +18,6 @@ const LandingPage = () => {
           <Announcement />
           <Hero />
           <Showcase />
-          {/* <ImageRevealOnScroll /> */}
-          {/* <div className="relative">
-            <Gallery />
-          </div> */}
           <Footer />
         </div>
       </div>
